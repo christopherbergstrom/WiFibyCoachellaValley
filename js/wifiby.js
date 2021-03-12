8ff6476da8a9df2c0d3d1f0db909bce9d8ba72f8
Christopher Bergstrom <christopherrbergstrom@gmail.com>
2021-03-11 18:49:28 -0800

    added video header, updated plans and info

--- js/wifiby.js
+++ js/wifiby.js
@@ -1,28 +1,54 @@
-$(document).ready(function()
-{
-  // scrolling
-  $("#servicesBtn").click(function()
-  {
-    var scrollAmount = $("#services").offset().top;
-    $('html,body').animate({scrollTop: scrollAmount}, 1000);
-  });
-  $("#contactBtn").click(function()
-  {
-    var scrollAmount = $("#contact").offset().top;
-    $('html,body').animate({scrollTop: scrollAmount}, 1000);
-  });
-  $("#privacy").click(function()
-  {
-    event.preventDefault();
-    alert("Privacy Policy\n\nInformation collected:\nName, Address, Telephone Number, Email Address and Payment Information\n\nUse of information:\nInformation is used to process payment for internet service and/or equipment and any add-ons.\n\nShared Information:\nInformation is shared with credit card processing company only to process monthly payments. No one else.\n\nOpting Out:\nPlease contact office at 760-325-4040\n");
-  });
-  $(".menuBtn1").click(function()
-  {
-      console.log("here");
-      $("#mySidebar").css("display", "block");
-  });
-  $(".menuBtn2").click(function()
-  {
-      $("#mySidebar").css("display", "none");
-  });
-});
+// $(document).ready(function () {
+window.onload = function () {
+  function count() {
+    console.log("in count");
+    let countItems = document.querySelectorAll(".count");
+    console.log(countItems[0]);
+    console.log(countItems[1]);
+    console.log(countItems[2]);
+    countItems.forEach(function (item) {
+      let i = 0
+      let max = parseInt(item.innerHTML);
+      // item.innerHTML = i;
+      let countInterval = (function () {
+        console.log("here");
+        item.innerHTML = i;
+        i++;
+        if (i == max) clearInterval(countInterval);
+      }, 1);
+      // for (let i = 0; i < max + 1; i++) {
+      // }
+    });
+  }
+
+  var element;
+  var windowHeight;
+
+  function init() {
+    element = document.querySelector('#count-container');
+    windowHeight = window.innerHeight;
+  }
+
+  function checkPosition() {
+    // for (var i = 0; i < elements.length; i++) {
+    //   var element = elements[i];
+    // var positionFromTop = elements[i].getBoundingClientRect().top;
+    var positionFromTop = element.getBoundingClientRect().top;
+    // console.log(positionFromTop);
+    // console.log(windowHeight);
+    console.log(positionFromTop - windowHeight);
+    if (positionFromTop - windowHeight <= 0) {
+      count();
+      // element.classList.add('fade-in-element');
+      // element.classList.remove('hidden');
+    }
+    // }
+  }
+
+  window.addEventListener('scroll', checkPosition);
+  window.addEventListener('resize', init);
+
+  init();
+  checkPosition();
+  // });
+}
